d55c1230492a460c50f67f95ba5c23a67f7bcc16
RichiePhetasa <Richiep559@mail.fresnostate.edu>
2019-05-16 01:52:30 -0700

    added functions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiURL: 'http://localhost:3002/api/v1'
 };
 
 /*
@@ -12,4 +13,4 @@ export const environment = {
  * below file. Don't forget to comment it out in production mode
  * because it will have a performance impact when errors are thrown
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+ import 'zone.js/dist/zone-error';  // Included with Angular CLI.
